a6229952a555c19a5e5f2bbb147eb0e5260b86c1
Kai Wang <charles.wangkai@gmail.com>
2015-06-12 19:32:35 +0800

    Reverse Complement Problem

---
+++ 1b.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+COMPLEMENTS = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'};
+
+def reverse_complement(pattern):
+    return ''.join(reversed(list(map(lambda x: COMPLEMENTS[x], pattern))))
+
+def main():
+    pattern = input()
+    print(reverse_complement(pattern))
+
+if __name__ == '__main__':
+    main()
